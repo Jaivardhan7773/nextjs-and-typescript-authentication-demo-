d53afbb1c4b5bc50c5a4a60978ac5e3d9035573b
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-09-03 00:01:24 +0530

    Route secured using zustand state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,18 @@ import { useRouter } from 'next/navigation';
 import { useAuthStore } from "./lib/store";
 
 export default function Home() {
+  const router = useRouter();
+
+  const {user} = useAuthStore()
+
+
+  useEffect(() => {
+    if (!user) {
+      router.replace('/auth/login'); // redirect to login if not logged in
+    }
+  }, [user, router]);
+
+  if (!user) return null
 
 
   return (
